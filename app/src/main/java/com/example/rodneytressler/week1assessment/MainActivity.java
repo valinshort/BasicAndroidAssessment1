661ffcd9457191fcbe802a8bcfb9741871421a70
Valin Short <valin.short1998@gmail.com>
2018-03-19 16:40:12 -0400

    assesment oneandroid studio

--- app/src/main/java/com/example/rodneytressler/week1assessment/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/week1assessment/MainActivity.java
@@ -9,10 +9,35 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textviewm;
+    private EditText edittextm;
+    private Button buttonm;
+    private String InputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textviewm = findViewById(R.id.textView);
+        edittextm = findViewById(R.id.edit_text);
+        buttonm = findViewById(R.id.button);
+
+
+        setListener() {
+
+
+        }
+
+    private void setListener() {
+        buttonm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = get.edittextm;
+                set.inputText("");
+            }
+        });
     }
 }
+
